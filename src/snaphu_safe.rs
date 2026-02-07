b6d62fb42cc41944efd3275375b1b181a9e7d3d8
Vinícius Freitas <vinifreitas.d.a@gmail.com>
2026-02-07 02:23:55 -0300

    up_node_scan implementation (WIP)

--- src/snaphu_safe.rs
+++ src/snaphu_safe.rs
@@ -9,6 +9,7 @@ const PRICE_MIN: f64 = 0.0;
 
 pub type NodeIndex = usize;
 pub type ArcIndex = usize;
+pub type BucketIndex = usize;
 
 #[derive(Copy, Clone)]
 #[repr(C)]
@@ -40,16 +41,89 @@ pub struct Node {
     /// Previous node in a bucket-list
     pub b_prev: NodeIndex,
     /// Bucket number
-    pub rank: u64,
+    pub rank: i64,
     /// Excess of the node
-    pub excess: u64,
+    pub excess: i64,
     /// Temporary number of input arcs
     pub inp: i8,
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct Bucket {
+    pub p_first: NodeIndex,
 }
 
 pub struct Graph {
     pub nodes: Vec<Node>,
     pub arcs: Vec<Arc>,
+}
+
+pub fn up_node_scan(
+    nodes: &mut [Node],
+    arcs: &[Arc],
+    buckets: &mut [Bucket],
+    current_bucket: BucketIndex,
+    i: NodeIndex,
+    n_scan: &mut usize,
+    dn: f64,
+    epsilon: f64,
+    linf: i64,  /* number of l_bucket + 1 */
+    dlinf: f64, /* copy of linf in double mode */
+) {
+    let mut j: NodeIndex; /* opposite node */
+    let mut b_old: BucketIndex; /* old bucket contained j */
+    let mut b_new: BucketIndex; /* new bucket for j */
+    let mut j_rank: i64; /* ranks of nodes */
+    let mut j_new_rank: i64;
+    let i_rank: i64 = nodes[i].rank;
+    let mut rc: f64; /* reduced cost of (j,i) */
+    let mut dr: f64; /* rank difference */
+    let mut a: ArcIndex = nodes[i].first; /* ( i, j ) */
+    let a_stop: ArcIndex = nodes[i + 1].suspended; /* first arc from the next node */
+    *n_scan += 1;
+    while a != a_stop {
+        let ra: ArcIndex = arcs[a].sister; /* ( j, i ) */
+        if arcs[ra].r_cap > 0 {
+            j = arcs[a].head as NodeIndex;
+            j_rank = nodes[j].rank;
+            if j_rank > i_rank {
+                rc = nodes[j].price + dn * arcs[ra].cost as f64 - nodes[i].price;
+                if rc < 0 as f64 {
+                    j_new_rank = i_rank;
+                } else {
+                    dr = rc / epsilon;
+                    j_new_rank = if dr < dlinf {
+                        i_rank + dr as i64 + 1
+                    } else {
+                        linf
+                    };
+                }
+                if j_rank > j_new_rank {
+                    nodes[j].rank = j_new_rank;
+                    nodes[j].current = ra;
+                    if j_rank < linf {
+                        b_old = (current_bucket as i64 + j_rank) as BucketIndex;
+                        // TODO: reevaluate this REMOVE_FROM_BUCKET
+                        if j == buckets[b_old].p_first {
+                            buckets[b_old].p_first = nodes[j].b_next as NodeIndex;
+                        } else {
+                            nodes[nodes[j].b_prev].b_next = nodes[j].b_next;
+                            nodes[nodes[j].b_next].b_prev = nodes[j].b_prev;
+                        }
+                    }
+                    b_new = (current_bucket as i64 + j_new_rank) as BucketIndex;
+                    // TODO: reevaluate this INSERT_TO_BUCKET
+                    nodes[j].b_next = buckets[b_new].p_first;
+                    nodes[buckets[b_new].p_first].b_prev = j;
+                    buckets[b_new].p_first = j;
+                }
+            }
+        }
+        a = a + 1;
+    }
+    nodes[i].price -= i_rank as f64 * epsilon;
+    nodes[i].rank = -1;
 }
 
 /// Relabels a node of index `i`.
@@ -60,10 +134,10 @@ pub fn relabel(
     price_min: f64,
     dn: f64,
     epsilon: f64,
-    n_ref: u64,
+    n_ref: i64,
     flag_price: &mut i32,
-    n_relabel: &mut u64,
-    n_rel: &mut u64,
+    n_relabel: &mut i64,
+    n_rel: &mut i64,
 ) -> bool {
     // current arc from i
     let mut a: ArcIndex = nodes[i].current + 1;
